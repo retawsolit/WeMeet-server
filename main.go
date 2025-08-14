6a630bfaefaf9bf938261ce5392d481a2ba5c2c7
retawsolit <pykepyke147@gmail.com>
2025-08-14 15:01:40 +0700

    feat(server): basic HTTP /health using loaded port

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/retawsolit/WeMeet-server/pkg/config"
+)
 
 func main() {
-	log.Println("WeMeet API bootstrapped")
+	// Trong dev: copy config.yaml đã có sẵn ở root khi chạy app
+	cfg, err := config.Load("config.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	addr := fmt.Sprintf(":%d", cfg.Client.Port)
+	log.Println("HTTP listening on", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
