a90defb854bdbbd3825ce86cd7b911b549ecec87
retawsolit <pykepyke147@gmail.com>
2025-09-03 14:25:41 +0700

    nats

---
+++ pkg/models/nats_system.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"fmt"
+	"github.com/mynaparrot/plugnmeet-protocol/auth"
+	"github.com/mynaparrot/plugnmeet-protocol/plugnmeet"
+	natsservice "github.com/mynaparrot/plugnmeet-server/pkg/services/nats"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
+func (m *NatsModel) RenewPNMToken(roomId, userId, token string) {
+	// to renew token, we can avoid it to check expiry
+	// because in may case because of network related issues,
+	// the client was not able to renew token
+	// as renew request is coming from nats, so it should be secure
+	token, err := m.authModel.RenewPNMToken(token, false)
+	if err != nil {
+		log.Errorln(fmt.Errorf("error renewing pnm token for %s; roomId: %s; msg: %s", userId, roomId, err.Error()))
+		return
+	}
+
+	err = m.natsService.BroadcastSystemEventToRoom(plugnmeet.NatsMsgServerToClientEvents_RESP_RENEW_PNM_TOKEN, roomId, token, &userId)
+	if err != nil {
+		log.Errorln(fmt.Errorf("error sending RESP_RENEW_PNM_TOKEN event for %s; roomId: %s; msg: %s", userId, roomId, err.Error()))
+	}
+}
+
+func (m *NatsModel) GenerateLivekitToken(roomId string, userInfo *plugnmeet.NatsKvUserInfo) (string, error) {
+	c := &plugnmeet.PlugNmeetTokenClaims{
+		RoomId:  roomId,
+		Name:    userInfo.Name,
+		UserId:  userInfo.UserId,
+		IsAdmin: userInfo.IsAdmin,
+	}
+
+	return auth.GenerateLivekitAccessToken(m.app.LivekitInfo.ApiKey, m.app.LivekitInfo.Secret, *m.app.Client.TokenValidity, c)
+}
+
+func (m *NatsModel) HandleClientPing(roomId, userId string) {
+	// check user status
+	// if we found offline/disconnected, then we'll update
+	//  because the server may receive this join status a bit lately
+	// as user has sent ping request, this indicates the user is online
+	// OnAfterUserJoined will check the current status and act if the user was not online.
+	m.OnAfterUserJoined(roomId, userId)
+
+	err := m.natsService.UpdateUserKeyValue(roomId, userId, natsservice.UserLastPingAt, fmt.Sprintf("%d", time.Now().UnixMilli()))
+	if err != nil {
+		log.Errorln(fmt.Sprintf("error updating user last ping for %s; roomId: %s; msg: %s", userId, roomId, err.Error()))
+	}
+}
---
+++ pkg/models/nats_user_event.go
@@ -0,0 +1,66 @@
+package models
+
+import (
+	"fmt"
+	"github.com/mynaparrot/plugnmeet-protocol/plugnmeet"
+	log "github.com/sirupsen/logrus"
+	"strings"
+)
+
+func (m *NatsModel) HandleInitialData(roomId, userId string) {
+	// send room info
+	rInfo, err := m.natsService.GetRoomInfo(roomId)
+	if err != nil {
+		log.Errorln(fmt.Sprintf("error getting room info for userId: %s, roomId: %s, msg: %s", userId, roomId, err.Error()))
+		_ = m.natsService.NotifyErrorMsg(roomId, err.Error(), &userId)
+		return
+	}
+	if rInfo == nil {
+		_ = m.natsService.NotifyErrorMsg(roomId, "room information not found", &userId)
+	}
+
+	// send this user's info
+	userInfo, err := m.natsService.GetUserInfo(roomId, userId)
+	if err != nil {
+		log.Errorln(fmt.Sprintf("error getting user info for userId: %s, roomId: %s, msg: %s", userId, roomId, err.Error()))
+		_ = m.natsService.NotifyErrorMsg(roomId, err.Error(), &userId)
+		return
+	}
+	if userInfo == nil {
+		_ = m.natsService.NotifyErrorMsg(roomId, "no user found", &userId)
+		return
+	}
+
+	// send media server connection info
+	token, err := m.GenerateLivekitToken(roomId, userInfo)
+	if err != nil {
+		log.Errorln(err)
+		_ = m.natsService.NotifyErrorMsg(roomId, err.Error(), &userId)
+		return
+	}
+	lkHost := strings.Replace(m.app.LivekitInfo.Host, "host.docker.internal", "localhost", 1) // without this you won't be able to connect
+
+	initial := &plugnmeet.NatsInitialData{
+		Room:      rInfo,
+		LocalUser: userInfo,
+		MediaServerInfo: &plugnmeet.MediaServerConnInfo{
+			Url:   lkHost,
+			Token: token,
+		},
+	}
+
+	// send important info first
+	err = m.natsService.BroadcastSystemEventToRoom(plugnmeet.NatsMsgServerToClientEvents_RES_INITIAL_DATA, roomId, initial, &userId)
+	if err != nil {
+		log.Warnln(fmt.Sprintf("error sending RES_INITIAL_DATA event userId: %s, roomId: %s, msg: %s", userId, roomId, err.Error()))
+	}
+}
+
+func (m *NatsModel) HandleSendUsersList(roomId, userId string) {
+	if users, err := m.natsService.GetOnlineUsersListAsJson(roomId); err == nil && users != nil {
+		err := m.natsService.BroadcastSystemEventToRoom(plugnmeet.NatsMsgServerToClientEvents_RES_JOINED_USERS_LIST, roomId, users, &userId)
+		if err != nil {
+			log.Warnln(fmt.Sprintf("error sending RES_JOINED_USERS_LIST event userId: %s, roomId: %s, msg: %s", userId, roomId, err.Error()))
+		}
+	}
+}
